f82d414872b9b2470570f6da48285680e8cfa7f5
Yreawl <yreawl@gmail.com>
2024-07-05 08:37:59 +0100

    Create Clear-USBGhostDevices.ps1

---
+++ Clear-USBGhostDevices.ps1
@@ -0,0 +1,23 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2024 v5.8.244
+	 Created on:   	05/07/2024 08:37
+	 Created by:   	yreaw
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+# https://superuser.com/questions/1776942/how-to-bulk-remove-all-hidden-devices-in-device-manager-sample-powershell-scri
+
+#List all hidden devices
+$unknown_devs = Get-PnpDevice | Where-Object{ $_.Status -eq 'Unknown' }
+
+#loop through all hidden devices to remove them using pnputil
+ForEach ($dev in $unknown_devs)
+{
+	pnputil /remove-device $dev.InstanceId
+}
