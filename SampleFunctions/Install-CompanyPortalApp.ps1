693d131b58b0b18503e97b5110016f4c34752d9d
Yreawl <95140166+Yreawl@users.noreply.github.com>
2024-10-01 12:59:19 +0100

    Create Install-CompanyPortalApp.ps1

---
+++ SampleFunctions/Install-CompanyPortalApp.ps1
@@ -0,0 +1,13 @@
+﻿function Install-CompanyPortalApp
+{
+	param (
+		[Parameter(Mandatory = $True)]
+		[string]$ApplicationId
+	)
+	
+	start-process companyportal:ApplicationId=$ApplicationId
+	Start-Sleep 10
+	[void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+	[System.Windows.Forms.SendKeys]::SendWait("^{i}")
+	
+}
